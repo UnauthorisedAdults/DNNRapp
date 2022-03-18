52472f920d718be0c669fdd17e290c20f4ec4f98
Line Guld <linekguld@hotmail.com>
2022-03-18 10:38:47 +0100

    Initial commit

---
+++ app/src/main/java/com/unauthorisedadults/dnnr/MainActivity.java
@@ -0,0 +1,14 @@
+package com.unauthorisedadults.dnnr;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
