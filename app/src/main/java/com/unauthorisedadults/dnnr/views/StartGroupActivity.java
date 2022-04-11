3e2219739bf4f00d46fc8b0e013166bd78b4421e
LineGuld <linekguld@hotmail.com>
2022-04-11 14:07:52 +0200

    Gruppe venteværelse

--- app/src/main/java/com/unauthorisedadults/dnnr/views/StartGroupActivity.java
+++ app/src/main/java/com/unauthorisedadults/dnnr/views/StartGroupActivity.java
@@ -1,5 +1,45 @@
 package com.unauthorisedadults.dnnr.views;
 
-public class StartGroupActivity {
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import com.unauthorisedadults.dnnr.R;
+
+public class StartGroupActivity extends AppCompatActivity {
+
+    TextView groupIdDisplay, groupMemberBox;
+    Button start, cancel;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.groupactivity);
+
+        Toolbar toolbar = findViewById(R.id.DNNR_Toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setIcon(R.drawable.ic_logo_smaller);
+        getSupportActionBar().setTitle("");
+
+
+        groupIdDisplay = findViewById(R.id.GroupIdDisplay);
+        groupMemberBox = findViewById(R.id.GroupMembersBox);
+        start = findViewById(R.id.startButton);
+        cancel = findViewById(R.id.cancelButton);
+    }
+
+    public void startSwiping(View view) {
+        //viewmodel.start
+        //Noget med et intent
+    }
+
+    public void cancelGroup(View view)
+    {
+        //viewModel.canselgroup
+        //Intent sender bruger tilbage til home
+    }
 }
