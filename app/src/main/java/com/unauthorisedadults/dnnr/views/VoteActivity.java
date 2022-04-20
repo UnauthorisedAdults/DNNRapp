f3ca2aa23825c1f2f8cfe2603b95fcf660a8d275
Line Guld <linekguld@hotmail.com>
2022-04-20 14:54:05 +0200

    cardstack virker ikke engang en lille smule

---
+++ app/src/main/java/com/unauthorisedadults/dnnr/views/VoteActivity.java
@@ -0,0 +1,9 @@
+package com.unauthorisedadults.dnnr.views;
+
+import androidx.appcompat.app.AppCompatActivity;
+import com.daprlabs.cardstack.SwipeDeck;
+
+public class VoteActivity extends AppCompatActivity {
+
+
+}
