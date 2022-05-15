f9bd101231b6660f416a1b645c2b9cd916dc24e4
LineGuld <linekguld@hotmail.com>
2022-05-15 12:24:28 +0200

    Der mangler noget payload noget
    https://developers.google.com/nearby/connections/android/exchange-data

--- app/src/main/java/com/unauthorisedadults/dnnr/utilities/UTIL.java
+++ app/src/main/java/com/unauthorisedadults/dnnr/utilities/UTIL.java
@@ -4,5 +4,6 @@ public class UTIL {
 
     public static String USER = "User";
     public static String HUB_URI = "https://businessserver20220511100035.azurewebsites.net/";
+    public static String SERVICE_ID = "com.unauthorisedadults.dnnr";
 
 }
